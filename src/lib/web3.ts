8782fe21233a60465cb2ea68a9e31a260154fc3c
César Guadarrama <cesargdm@icloud.com>
2022-10-24 11:57:21 -0500

    Update alchemy api key and use testnets

--- src/lib/web3.ts
+++ src/lib/web3.ts
@@ -1,22 +1,25 @@
-import { getDefaultWallets } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 
 const { chains, provider } = configureChains(
-	[chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-	[alchemyProvider({ apiKey: import.meta.env.ALCHEMY_ID }), publicProvider()]
-);
+	[chain.goerli, chain.polygonMumbai],
+	[
+		alchemyProvider({ apiKey: import.meta.env.VITE_APP_ALCHEMY_ID }),
+		publicProvider(),
+	],
+)
 
 const { connectors } = getDefaultWallets({
-	appName: "My RainbowKit App",
+	appName: 'My RainbowKit App',
 	chains,
-});
+})
 
 const wagmiClient = createClient({
 	autoConnect: true,
 	connectors,
 	provider,
-});
+})
 
-export { wagmiClient, chains };
+export { wagmiClient, chains }
