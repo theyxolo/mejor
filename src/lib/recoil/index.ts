54a7a04dc9149d9535c5796bdc375ef8fb22477e
César Guadarrama <cesargdm@icloud.com>
2022-11-03 20:31:00 -0600

    Fixes

--- src/lib/recoil/index.ts
+++ src/lib/recoil/index.ts
@@ -23,7 +23,11 @@ const localStorageEffect: (key: string) => AtomEffect<string | null> =
 	({ setSelf, onSet }) => {
 		const savedValue = localStorage.getItem(key)
 		if (savedValue != null) {
-			setSelf(JSON.parse(savedValue))
+			if (typeof savedValue === 'string') {
+				setSelf(savedValue)
+			} else {
+				setSelf(JSON.parse(savedValue))
+			}
 		}
 
 		onSet((newValue, _, isReset) =>
